c0c146a2f1123b7f0cf96ec95f1ec836f89c8a28
Harley Lin <harley9387@gmail.com>
2025-06-16 15:57:07 +1200

    try pwa, check if it works on production

---
+++ src/app/manifest.js
@@ -0,0 +1,23 @@
+export default function manifest() {
+  return {
+    name: "TGC 抽奖",
+    short_name: "抽奖",
+    description: "TGC 门店线下抽奖APP",
+    start_url: "/store",
+    display: "standalone",
+    background_color: "#ffffff",
+    theme_color: "#54C8E8",
+    icons: [
+      {
+        src: "/icon-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        src: "/icon-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+      },
+    ],
+  };
+}
